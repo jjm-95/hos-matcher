6425d7b867ec779f757149e673d1a241a6248fcc
전재명 <jeonjm0130@naver.com>
2025-04-23 16:57:58 +0900

    favicon, title modify

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
+
+export default nextConfig;
